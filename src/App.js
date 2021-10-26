ef058d7870ddde682906c9ed1a39556ca8bf9fe7
Rudy Prevaud <kanachou@pop-os.localdomain>
2021-10-26 09:38:50 +0200

    Make discover quest

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+const GET_LAUNCHES = gql`
+  query getLaunches {
+    launches(limit: 5) {
+      launch_date_utc
+      launch_success
+      rocket {
+        rocket_name
+      }
+      links {
+        video_link
+      }
+      details
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_LAUNCHES);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <h1>error :(</h1>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
